9cec02756e629a1b8451808285e3f5d994d210a3
Jose Andres Solano Vargas <josesolanovarga13@gmail.com>
2024-11-19 16:34:30 -0600

    Actualizacion de gamecontroll

--- backend/src/controllers/gameControllers.ts
+++ backend/src/controllers/gameControllers.ts
@@ -1,5 +1,5 @@
 import { Server, Socket } from 'socket.io';
-import { Game, CreateGameData, JoinGameData } from './types/game';
+import { Game, CreateGameData, JoinGameData } from '../types/game';
 
 let games: Game[] = [];
 
