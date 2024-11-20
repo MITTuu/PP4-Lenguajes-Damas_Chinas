ed3b70106c1cc977de0a02606e7dcbb28c847a7e
hillarymalespin <hillarymalespin@estudiantec.cr>
2024-11-20 00:01:48 -0600

    Update

--- frontend/src/components/Inicio/UnirsePartida.jsx
+++ frontend/src/components/Inicio/UnirsePartida.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { socketManager } from '../../services/socketManager';
+import "../../assets/unirsePartida.css";
 
 const UnirsePartida = () => {
     const navigate = useNavigate();
